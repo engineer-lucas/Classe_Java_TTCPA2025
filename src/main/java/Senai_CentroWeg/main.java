1973a72d50577914c2608cc8a002de78b085310e
Lucas Santos <54923712+engineer-lucas@users.noreply.github.com>
2025-10-01 11:14:32 -0300

    Update main.java
    Configurando visibilidades

--- src/main/java/Senai_CentroWeg/main.java
+++ src/main/java/Senai_CentroWeg/main.java
@@ -1,38 +1,34 @@
 package Senai_CentroWeg;
 
-public class main {
-    public static void main(String[] args) {
-        System.out.println("------------------");
-        System.out.println("OBJETO 1");
-        Caneta c1 = new Caneta();
-        c1.modelo = "Compactor 07";
-        c1.cor = "Azul";
-        c1.ponta = 0.5f;
-        c1.tampada = false;
-        c1.tampar();
-        c1.status();
-        c1.rabiscar();
+public class Caneta {
+    //Atributos da caneta, ou seja, suas caracteristicas.
+    //O que eu tenho ?
+    public String modelo;
+   public String cor;
+   private float ponta;
+   protected int carga;
+   private boolean tampada;
 
-        System.out.println("------------------");
-        System.out.println("OBJETO 2");
-        Caneta c2 = new Caneta();
-        c2.modelo = "Bic Cristal";
-        c2.cor = "Preta";
-        c2.ponta = 0.9f;
-        c2.tampada = true;
-        c2.destampar();
-        c2.status();
-        c2.rabiscar();
+//O que eu faço ? Métodos
+   public void status(){
+       System.out.println("Modelo " + this.modelo);
+       System.out.println("Uma caneta de cor " + this.cor);
+       System.out.println("Com a ponta " + this.ponta);
+       System.out.println("Com a carga " + this.carga);
+       System.out.println("Está tampada ? " + this.tampada);
+   }
+   public void rabiscar(){
+       if (this.tampada == true) {
+           System.out.println("Não posso rabiscar, pois estou tampada!");
+       } else {
+           System.out.println("Estou rabiscando, pois estou destampada!");
+       }
+   }
+   protected void tampar(){
+       this.tampada = true;
+   }
+   public void destampar(){
+       this.tampada = false;
+   }
 
-        System.out.println("------------------");
-        System.out.println("OBJETO 3");
-        Caneta c3 = new Caneta();
-        c3.modelo = "Faber Castel";
-        c3.cor = "Vermelha";
-        c3.ponta = 0.7f;
-        c3.destampar();
-        c3.status();
-        c3.rabiscar();
-
-    }
 }
