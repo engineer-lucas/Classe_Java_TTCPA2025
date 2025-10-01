b185c57012e65d876d0b485a0e2e20c2c7397451
Lucas Santos <54923712+engineer-lucas@users.noreply.github.com>
2025-10-01 11:15:43 -0300

    Update Caneta.java
    Configurand visibilidade

--- src/main/java/Senai_CentroWeg/Caneta.java
+++ src/main/java/Senai_CentroWeg/Caneta.java
@@ -1,30 +1,33 @@
 package Senai_CentroWeg;
 
 public class Caneta {
-   String modelo;
-   String cor;
-   float ponta;
-   int carga;
-   boolean tampada;
+    //Atributos da caneta, ou seja, suas caracteristicas.
+    //O que eu tenho ?
+   public String modelo;
+   public String cor;
+   private float ponta;
+   protected int carga;
+   private boolean tampada;
 
-   void status(){
+//O que eu faço ? Métodos
+   public void status(){
        System.out.println("Modelo " + this.modelo);
        System.out.println("Uma caneta de cor " + this.cor);
        System.out.println("Com a ponta " + this.ponta);
        System.out.println("Com a carga " + this.carga);
        System.out.println("Está tampada ? " + this.tampada);
    }
-   void rabiscar(){
+   public void rabiscar(){
        if (this.tampada == true) {
            System.out.println("Não posso rabiscar, pois estou tampada!");
        } else {
            System.out.println("Estou rabiscando, pois estou destampada!");
        }
    }
-   void tampar(){
+   protected void tampar(){
        this.tampada = true;
    }
-   void destampar(){
+   public void destampar(){
        this.tampada = false;
    }
 
